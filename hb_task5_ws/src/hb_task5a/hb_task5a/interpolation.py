51eba296099cbf3568e9bc6fadd1086e51e44271
AKSHAR DASH <dashakshar376@gmail.com>
2024-01-26 23:46:04 +0530

    ik

---
+++ hb_task5_ws/src/hb_task5a/hb_task5a/interpolation.py
@@ -0,0 +1,85 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Pose2D
+from geometry_msgs.msg import Wrench
+from nav_msgs.msg import Odometry
+import time
+import math
+import numpy as np
+from std_msgs.msg import Bool
+from tf_transformations import euler_from_quaternion
+from geometry_msgs.msg import Twist
+from my_robot_interfaces.msg import Goal             
+from std_msgs.msg import Float64MultiArray
+import numpy as np
+import matplotlib.pyplot as plt
+
+rpmValues_right = [-42.55, -40.26, -35.714, -30.76, -21.66, -13.53, -10.60, -9.81, 0, 0, 0, 10.638, 13.495, 16.80, 25, 34.09, 42.55, 45.24, 46.9]
+pwmValues_right = [180, 165, 150, 135, 120, 105, 100, 97, 96, 90, 88, 87, 80, 75, 60, 45, 30, 15, 0]
+
+
+# Remove duplicate RPM values
+unique_indices = np.unique(rpmValues_right, return_index=True)[1]
+unique_rpm = np.array(rpmValues_right)[unique_indices]
+unique_pwm = np.array(pwmValues_right)[unique_indices]
+
+# Sort the data
+sorted_indices = np.argsort(unique_rpm)
+sorted_rpm_right = unique_rpm[sorted_indices]
+sorted_pwm_right = unique_pwm[sorted_indices]
+
+
+
+
+class interp(Node):
+    def __init__(self):
+        super().__init__('interpolator1')
+        
+        self.mapper = self.create_subscription(Float64MultiArray,'/map', self.mapCallBack1, 10)
+        # self.w_max=0.0
+        self.rate = self.create_rate(100)
+    
+    def mapCallBack1(self, msg1):
+        global sorted_pwm_right,sorted_rpm_right
+        self.w1=msg1.data[0]
+        self.w2=msg1.data[1]
+        self.w3=msg1.data[2]
+
+        self.pwm_right=np.interp(self.w1, sorted_rpm_right, sorted_pwm_right)
+
+
+        
+
+
+
+    
+    
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+    
+    # Create an instance of the HBController class
+    hb_controller = interp()
+
+    # Main loop
+    while rclpy.ok():
+        # Check if the service call is done
+        
+                ####################################################
+        # hb_controller.get_logger().info("GOAL: no ")
+        # Spin once to process callbacks
+        rclpy.spin_once(hb_controller)
+    
+    # Destroy the node and shut down ROS
+    hb_controller.destroy_node()
+    rclpy.shutdown()
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()    
--- micro_ros_subscriber_wifi/micro_ros_subscriber_wifi_1/micro_ros_subscriber_wifi_1.ino
+++ micro_ros_subscriber_wifi/micro_ros_subscriber_wifi_1/micro_ros_subscriber_wifi_1.ino
@@ -6,9 +6,8 @@
 #include <rclc/executor.h>
 #include <ESP32Servo.h>
 #include <InterpolationLib.h>
-//#include <Servo.h>
 #include <geometry_msgs/msg/twist.h>
-///
+
 
 rcl_subscription_t subscriber;
 geometry_msgs__msg__Twist msg;
@@ -23,21 +22,13 @@ Servo servo3;
 Servo micro_servo;
 
 int targetRPM=10;
-double rpmValues_right[19] = {-42.55,   -40.26   , -35.714, -30.76, -21.66, -13.53, -10.60, -9.81, 0  ,0,0, 10.638,13.495,16.80,25, 34.09,42.55,45.24,46.9}; //rpm
-double pwmValues_right[19] = {180   ,     165  ,   150    , 135 ,  120,     105,   100,    97,   96,90, 88,  87,    80,    75,  60,  45,   30,   15,   0}; //pwm
 
-//int constrainedRPM = constrain(abs(targetRPM), 0, 60);
-//int PWMVal = Interpolation::SmoothStep(rpmValues, pwmValues, 9, constrainedRPM);
 
 // Defining pins
 #define servo1_pin  26 //right
 #define servo2_pin  25  //left
 #define servo3_pin  27 //rear
 #define servo_pin   19  //pen
-//int minRPM = -57.6;   /   // Minimum RPM supported by the servo
-//int maxRPM = 57.6; /      // Maximum RPM supported by the servo
-//int SERVO_MIN_PWM = 2500; // Minimum PWM value accepted by the servo/
-//int SERVO_MAX_PWM = 500;// Maximum PWM value accepted by the servo/
 
 #define LED_PIN 2
 double w1_vel=0;
@@ -87,52 +78,11 @@ void subscription_callback(const void * msgin)
   int angular_z= msg->angular.z;//
 
   
-//  int constrainedRPM_right = constrain(abs(linear_x), 0, 60);
-
-
-//  /int PWMVal = Interpolation::SmoothStep(rpmValues_right, pwmValues_right, 19, constrainedRPM_right);
-//  /Serial.println(PWMVal);
-  // Implement your control logic or kinematics here
-//  inverse_kinematics(linear_x, linear_y, linear_z);
-//  publish_vel(PWMVal, linear_y, linear_z);
 
   publish_vel(linear_x, linear_y, linear_z,angular_z);
 }
 
-//int normaliseRPM(int desiredRPM) {
-//  int pwmValue = map(desiredRPM, minRPM, maxRPM, SERVO_MIN_PWM, SERVO_MAX_PWM);
-//  return pwmValue;
-//}
 
-//int inverse_kinematics(double xvel, double yvel, double w){
-//  // Implement your inverse kinematics here
-////  w1_vel= (1/r)*(-0.33*xvel)+(0.58*yvel)+(0.0476*ang_vel);
-////  w2_vel= (1/r)*(-0.33*xvel)+(-0.58*yvel)+(0.0476*ang_vel);
-////  w3_vel= (1/r)*(0.66666*xvel)+(0.0476*ang_vel);
-//  w1_vel = (1/r) * ( (w*d) + (-0.5*xvel) + (0.866*yvel) );
-//  w2_vel = (1/r) * ( (w*d) + (-0.5*xvel) + (-0.866*yvel) );
-//  w3_vel = (1/r) * ( (w*d) + (1*xvel) + (0*yvel) );
-//  Serial.println(w1_vel);
-//  Serial.println(w2_vel);
-//  Serial.println(w3_vel);
-//
-//  int pwm1=normaliseRPM(w1_vel);
-//  int pwm2=normaliseRPM(w2_vel);
-//  int pwm3=normaliseRPM(w3_vel);
-//
-//  Serial.println(pwm1);
-//  Serial.println(pwm2);
-//  Serial.println(pwm3);
-//
-//  servo1.writeMicroseconds(pwm1);
-//  servo2.writeMicroseconds(pwm2);
-//  servo3.writeMicroseconds(pwm3);
-//  
-//  
-//  
-//  
-//  return 0;  // Return value as needed
-//}
 
 void setup() {
   servo_init();
