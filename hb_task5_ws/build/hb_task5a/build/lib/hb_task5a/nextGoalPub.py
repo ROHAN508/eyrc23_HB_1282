88a6fdbc60a02475a420e79ffcc35cb536dd0c98
AKSHAR DASH <dashakshar376@gmail.com>
2024-01-29 20:24:22 +0530

    bot2 interp

--- hb_task5_ws/build/hb_task5a/build/lib/hb_task5a/nextGoalPub.py
+++ hb_task5_ws/build/hb_task5a/build/lib/hb_task5a/nextGoalPub.py
@@ -41,22 +41,28 @@ class ServiceNode(Node):
 
         for angle in range(0, 121, self.resolution):
             t_rad = math.radians(angle)
-            x1 = (200 * math.cos(t_rad)) / self.scale
-            y1 = (150 * math.sin(4 * t_rad)) / self.scale
+            # x1 = (200 * math.cos(t_rad)) / self.scale
+            # y1 = (150 * math.sin(4 * t_rad)) / self.scale
+            x1 = ((200 * math.cos(t_rad)) / self.scale)+250
+            y1 = ((150 * math.sin(4 * t_rad)) / self.scale)+250
             coordinates_list1x.append(x1)
             coordinates_list1y.append(y1)
 
         for angle in range(120, 241, self.resolution):
             t_rad = math.radians(angle)
-            x2 = (200 * math.cos(t_rad)) / self.scale
-            y2 = (150 * math.sin(4 * t_rad)) / self.scale
+            # x2 = (200 * math.cos(t_rad)) / self.scale
+            # y2 = (150 * math.sin(4 * t_rad)) / self.scale
+            x2 = ((200 * math.cos(t_rad)) / self.scale)+250
+            y2 = ((150 * math.sin(4 * t_rad)) / self.scale)+250
             coordinates_list2x.append(x2)
             coordinates_list2y.append(y2)
 
         for angle in range(240, 361, self.resolution):
             t_rad = math.radians(angle)
-            x3 = (200 * math.cos(t_rad)) / self.scale
-            y3 = (150 * math.sin(4 * t_rad)) / self.scale
+            # x3 = (200 * math.cos(t_rad)) / self.scale
+            # y3 = (150 * math.sin(4 * t_rad)) / self.scale
+            x3 = ((200 * math.cos(t_rad)) / self.scale)+250
+            y3 = ((150 * math.sin(4 * t_rad)) / self.scale)+250
             coordinates_list3x.append(x3)
             coordinates_list3y.append(y3)
 
